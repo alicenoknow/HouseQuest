075a490321723f8fcc5bad53682d4c783ee68020
BiaPatatas <82343869+BiaPatatas@users.noreply.github.com>
2023-12-22 20:41:11 +0000

    dashboard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -48,29 +48,43 @@ const Dashboard: React.FC = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View>
+                <View style={styles.title}>
+                    <Text style={styles.title}>Dashboard</Text>
+                </View>
+                <View style={styles.avatarContainer}>
+                    <FlatList
+                        horizontal
+                        data={usersList}
+                        renderItem={renderUserAvatar}
+                        keyExtractor={(item) => item.id}
+                        style={styles.userList}
+                    />
+                </View>
+            </View>
+            <Text style={styles.subtitle}>Announcements</Text>
+            <View style={styles.line} />
+            <View style={styles.messagesSection}>
                 <FlatList
-                    horizontal
-                    data={usersList}
-                    renderItem={renderUserAvatar}
-                    keyExtractor={(item) => item.id}
                     style={styles.userList}
+                    data={announcementsList}
+                    renderItem={renderAnnouncement}
+                    keyExtractor={(item) => item.id}
                 />
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Type a message..."
+                    />
+                    <TouchableOpacity onPress={() => console.log('Send button pressed')}>
+                        <Text style={styles.sendButton}>Send</Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
-            <FlatList
-                style={styles.userList}
-                data={announcementsList}
-                renderItem={renderAnnouncement}
-                keyExtractor={(item) => item.id}
-            />
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Type a message..."
-                />
-                <TouchableOpacity onPress={() => console.log('Send button pressed')}>
-                    <Text style={styles.sendButton}>Send</Text>
-                </TouchableOpacity>
-            </View>
+
+
+
         </SafeAreaView>
     );
 };
@@ -80,17 +94,47 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: Spacers.xLarge,
     },
+    subtitle: {
+        color: Colors.darkGreen,
+
+        marginLeft: 10,
+        marginTop: 30,
+
+    },
+    messagesSection: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#E8E8E8',
+
+    },
+    line: {
+        width: '40%',
+        height: 0.6,
+        backgroundColor: Colors.darkGreen,
+        marginTop: 0,
+    },
+    avatarContainer: {
+        backgroundColor: Colors.darkGreen,
+        borderRadius: 50,
+        width: 300,
+        alignItems: 'center',
+        height: 55,
+        marginLeft: 45,
+    },
     announcementContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         margin: 10
+
     },
     userAvatar: {
-        borderRadius: 50,
-        backgroundColor: Colors.pink,
-        padding: Spacers.small,
-        margin: Spacers.small
+        borderRadius: 100,
+        padding: 5,
+        margin: 5,
+        height: 40,
+        marginTop: -3,
     },
+
     annoucementPhoto: {
         height: 220,
         width: 220,
@@ -102,29 +146,34 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         padding: 10,
+
     },
     input: {
-        flex: 1,
+        flex: 2,
         borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 10,
-        padding: 8,
+        borderColor: '#ccc',
+        marginRight: 10,
+        paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderRadius: 12,
+        height: 35,
     },
     sendButton: {
-        padding: 10,
-        marginHorizontal: 5,
         backgroundColor: Colors.darkGreen,
-        borderRadius: 10,
-        color: 'white',
+        borderRadius: Style.radius,
+        padding: Spacers.small,
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: "white",
     },
     messageText: {
         marginLeft: 10,
     },
     avatar: {
-        width: 50,
-        height: 50,
+        width: 30,
+        height: 30,
         borderRadius: 25,
-        margin: 5,
+        margin: 0,
     },
     userList: {
         padding: 10,
@@ -133,6 +182,15 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
     },
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: Colors.black,
+        marginLeft: 80,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+
 });
 
 export default Dashboard;
