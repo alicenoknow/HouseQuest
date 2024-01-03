ef43d052a578af8d96a94bdd610842bf549a08d8
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-01-03 01:06:43 +0000

    add/export auth to firebase config

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
-import { initializeApp } from "firebase/app";
-import {getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhe_raUxJKw-nWk0wXWTGYAwH__frKXhI",
-  authDomain: "housequest-15795.firebaseapp.com",
-  projectId: "housequest-15795",
-  storageBucket: "housequest-15795.appspot.com",
-  messagingSenderId: "353172267978",
-  appId: "1:353172267978:web:ee0872c0a9ed6c842ee533",
-  measurementId: "G-W0JZZ0E5R5"
+  apiKey: 'AIzaSyDhe_raUxJKw-nWk0wXWTGYAwH__frKXhI',
+  authDomain: 'housequest-15795.firebaseapp.com',
+  projectId: 'housequest-15795',
+  storageBucket: 'housequest-15795.appspot.com',
+  messagingSenderId: '353172267978',
+  appId: '1:353172267978:web:ee0872c0a9ed6c842ee533',
+  measurementId: 'G-W0JZZ0E5R5'
 };
 
-
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getFirestore(firebaseConfig);
