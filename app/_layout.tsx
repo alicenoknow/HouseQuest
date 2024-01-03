92d389ed701929b902e706ed07591a7541bec3b1
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-01-03 00:50:25 +0000

    add layout parent hide header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -51,6 +51,7 @@ function RootLayoutNav() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="users" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
       </UserProvider>
     </ThemeProvider>
