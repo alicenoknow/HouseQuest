8b97e8c739d0af366e344c86ca4da5a0974f6e7c
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-01-05 01:40:18 +0000

    remove header

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerTitle: 'Auth' }} />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
