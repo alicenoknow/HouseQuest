a02bbaf2c0a8233a6c693c63bc57eee444f97eb5
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-01-20 21:10:10 +0000

    LOGOUT_USER REMOVE_HOUSEHOLD

--- components/signoutGoogle.tsx
+++ components/signoutGoogle.tsx
@@ -4,8 +4,10 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 import { auth } from '../config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { router } from 'expo-router';
+import { useUserContext, UserActionType } from '../contexts/UserContext';
 
 const SignoutGoogle: React.FC = () => {
+  const { dispatch } = useUserContext();
   return (
     <View style={styles.container}>
       <Text>SignoutGoogle Component</Text>
@@ -15,6 +17,14 @@ const SignoutGoogle: React.FC = () => {
           await signOut(auth).then(async () => {
             AsyncStorage.removeItem('@user');
             AsyncStorage.removeItem('@household');
+            dispatch({
+              type: UserActionType.LOGOUT_USER,
+              user: null
+            });
+            dispatch({
+              type: UserActionType.REMOVE_HOUSEHOLD,
+              householdId: undefined
+            });
             router.replace('/auth');
           });
         }}
