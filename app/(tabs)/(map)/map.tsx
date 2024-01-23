5912a53afd7259f520d52f23c5f42d90230f261d
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-01-23 14:20:18 +0000

    save map. working?

--- app/(tabs)/(map)/map.tsx
+++ app/(tabs)/(map)/map.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View, Image } from 'react-native';
 import MapView, { LatLng, Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { useUserContext } from '../../../contexts';
-import { updateUserLocation } from '../../../remote/db';
+import { fetchMembers, updateUserLocation } from '../../../remote/db';
+import updateUserContextWithMembers from '../../../functions/updateUserContextWithMembers';
 import ShareLocationOverlay from './ShareLocationOverlay';
 import blueMarker from './blueMarker';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { User } from '../../../models';
 
 // TODO refactor, basically rewrite, extract components, fix styling
 async function requestPermissions() {
@@ -25,7 +27,7 @@ async function requestPermissions() {
 
 const Map: React.FC = () => {
   const { state: userState } = useUserContext();
-  const { user } = userState;
+  const { user, householdId, householdMembers } = userState;
   const userId = user?.id;
   const [currentLocation, setCurrentLocation] =
     useState<Location.LocationObject | null>(null);
@@ -33,33 +35,38 @@ const Map: React.FC = () => {
   const locationSubscription = useRef<Location.LocationSubscription | null>(
     null
   );
+  const [memberLocations, setMemberLocations] = useState<User[]>([]);
+  const memberUpdateTimer = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  useEffect(() => {
+    setMemberLocations([...householdMembers]);
+  }, [userState]);
+
+  useEffect(() => {
+    if (householdId) {
+      updateUserContextWithMembers();
+    }
+  }, [householdId]);
 
   useEffect(() => {
     requestPermissions();
 
-    // Define an async function inside the effect for handling the promise
-    const subscribe = async () => {
-      locationSubscription.current = await Location.watchPositionAsync(
-        { accuracy: Location.Accuracy.High, timeInterval: 30000 },
-        (newLocation) => {
-          console.log('New Location:', newLocation);
-          setCurrentLocation(newLocation);
-          if (userId) {
-            updateUserLocation(userId, {
-              latitude: newLocation.coords.latitude,
-              longitude: newLocation.coords.longitude
-            });
-          }
-        }
-      );
-    };
+    // Update user location every 30 seconds
+    Location.watchPositionAsync(
+      { accuracy: Location.Accuracy.High, timeInterval: 30000 },
+      (newLocation) => {
+        setCurrentLocation(newLocation);
+      }
+    );
 
-    subscribe(); // Call the async function
+    // Fetch and update member locations every 3 minutes
+    memberUpdateTimer.current = setInterval(() => {
+      updateUserContextWithMembers();
+    }, 180000); // 3 minutes
 
     return () => {
-      // Ensure to remove the subscription when the component unmounts
-      if (locationSubscription.current) {
-        locationSubscription.current.remove();
+      if (memberUpdateTimer.current) {
+        clearInterval(memberUpdateTimer.current);
       }
     };
   }, []);
@@ -103,6 +110,31 @@ const Map: React.FC = () => {
           description="Marker Description">
           <View>{blueMarker()}</View>
         </Marker>
+        {memberLocations
+          .filter(
+            (member) =>
+              member.location &&
+              member.location.latitude !== undefined &&
+              member.location.longitude !== undefined &&
+              member.location?.latitude &&
+              member.location?.longitude
+          )
+          .map((member) => (
+            <Marker
+              key={member.id}
+              coordinate={{
+                latitude: member.location!.latitude,
+                longitude: member.location!.longitude
+              }}
+              title={member.displayName}>
+              <View>
+                <Image
+                  source={{ uri: member.photoURL }}
+                  style={{ width: 30, height: 30, borderRadius: 15 }}
+                />
+              </View>
+            </Marker>
+          ))}
         {/* otherMarkers.map((marker) => ( */}
       </MapView>
       <MaterialCommunityIcons
